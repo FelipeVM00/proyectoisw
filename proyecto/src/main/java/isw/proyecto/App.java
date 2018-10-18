59e36e98dfc910d613549c4ac1dbd67e66b5e580
FelipVM <FelipVM@DESKTOP-O4L35F9>
2018-10-18 14:35:42 -0500

    Menu

---
+++ proyecto/src/main/java/isw/proyecto/App.java
@@ -0,0 +1,27 @@
+package isw.proyecto;
+
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application
+{
+	@Override
+	public void start(Stage stage) throws Exception {	
+		URL url = Thread.currentThread().getContextClassLoader ().getResource("menu.fxml");
+		System.out.println(url);
+		Parent root = FXMLLoader.load(url);
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		stage.setFullScreen(true);
+	}
+	public static void main( String[] args )
+	{
+	        launch(args);
+	}
+}
---
+++ proyecto/src/main/java/isw/proyecto/MenuController.java
@@ -0,0 +1,24 @@
+package isw.proyecto;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    private Button botonCaja;
+
+    @FXML
+    private Button botonGestInv;
+
+    @FXML
+    void clickBotonCaja(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickBotonGestInv(ActionEvent event) {
+
+    }
+}
