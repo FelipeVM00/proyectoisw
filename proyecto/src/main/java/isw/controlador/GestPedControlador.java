e338942eebd2df639e97f26536c41ebeef8cec79
FabioxUchiha <36249220+FabioxUchiha@users.noreply.github.com>
2018-10-23 10:01:52 -0500

    Add files via upload
    añadida opcion volver al menu y creacion de gestion de pedidos controlador

---
+++ proyecto/src/main/java/isw/controlador/GestPedControlador.java
@@ -0,0 +1,28 @@
+package isw.controlador;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class GestPedControlador {
+
+	@FXML
+    private Button botonVolver;
+	
+	@FXML
+    void clickBotonVolver(ActionEvent event) throws IOException {
+		Stage stage = (Stage) botonVolver.getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("/isw/vista/menu.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		stage.setFullScreen(true);
+    }
+
+}
